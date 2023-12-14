ca7ed9e40a20824585541e392079aef67405fa01
Aleksandr <a.shaddar@mail.ru>
2023-12-14 23:17:35 +0300

    Homework is done!

--- Seminar_7/homework.py
+++ Seminar_7/homework.py
@@ -45,11 +45,17 @@ else:
 # 2 4 6 
 # 3 6 9
 
-def print_operation_table(operation, num_rows, num_columns):
+def print_operation_table(operation, num_rows = 9, num_columns = 9):
     if num_rows < 2:
         print("ОШИБКА! Размерности таблицы должны быть больше 2!")
-    row = [i for i in range(1, num_rows + 1)]
-    colum = [i for i in range(1, num_columns + 1)]
+    else:
+        columns = [i for i in range(1, num_columns + 1)] 
+        print(*columns)
+        for i in range(2, num_rows + 1):
+            print(i, end='')
+            for j in range(2, num_columns + 1):
+                print(operation(i, j), end=' ')
+            print('\t')
 
 
 
--- Try.py
+++ Try.py
@@ -9,27 +9,17 @@
 # Если строк меньше двух, выдайте текст
 # ОШИБКА! Размерности таблицы должны быть больше 2!.
 
-
+    
 def print_operation_table(operation, num_rows = 9, num_columns = 9):
     if num_rows < 2:
         print("ОШИБКА! Размерности таблицы должны быть больше 2!")
     else:
-        for i in range(1, num_rows + 1):
-            print(i, end=' ')
-            for j in range(1, num_columns + 1):
+        columns = [i for i in range(1, num_columns + 1)] 
+        print(*columns)
+        for i in range(2, num_rows + 1):
+            print(i, end='')
+            for j in range(2, num_columns + 1):
                 print(operation(i, j), end=' ')
             print('\t')
-        return operation(num_rows, num_columns)
-    
- 
-# def print_operation_table(operation, num_rows, num_columns):
-#     if num_rows < 2:
-#         print("ОШИБКА! Размерности таблицы должны быть больше 2!")
-#     row = [i for i in range(1, num_rows + 1)]
-#     colum = [i for i in range(1, num_columns + 1)]    
-#     for i in row:
-#         for j in colum:
-#             print(operation(i, j), ' ', end='')
-#         print('')
 
-print_operation_table(lambda x, y: x + y, 7, 7)
+print_operation_table(lambda x, y: x * y, 3, 3)
